c1ed26ed8a6fc5aa0d27816d0d5e9f1ea4f663c9
Nenis <sylalp7@gmail.com>
2015-03-27 22:59:24 -0600

    Nuevas funciones en el main provenientes de userData.c

--- main.c
+++ main.c
@@ -4,9 +4,9 @@
 
 int main(int argc, char const *argv[]){
 
-	printf("__________________________________________________________ \n");
+	printf("______________________________________________________________________ \n");
 	printf("               DigitalSignatureSystem \n");
-  printf("__________________________________________________________ \n \n");
+	printf("______________________________________________________________________ \n");
   printf("Seleccione la operación que desea realizar\n\n");
   printf("1- Crear firma digital\n");
   printf("2- Revocar Cerficado\n");
@@ -14,25 +14,26 @@ int main(int argc, char const *argv[]){
   printf("4- Firma documento\n");
   printf("5- Validar documento\n\n");
 
-  int option;
+	int option;
   scanf("%d",&option);
   while (option != 1 && option != 2 && option != 3 &&
     option != 4 && option != 5){
       printf("Opcion inválida \n");
       printf("Seleccione la operación que desea realizar \n");
-      scanf("%d",&option);
+			scanf("%d",&option);
+			getchar();
   }
 	 switch(option)
 	  {
 		case 1:request_info_user();
 			break;
-		case 2:printf("OPICION2\n");
+		case 2:revoked_certificate();
 			break;
 		case 3:printf("OPICION3\n");
 			break;
-		case 4:printf("OPICION4\n");
+		case 4:signing_document();
 			break;
-    case 5:printf("OPICION5\n");
+    case 5:validate_document();
   		break;
 		}
 /*
--- userData.h
+++ userData.h
@@ -6,6 +6,9 @@
 #include <string.h>
 
 int pin_size(long num);
-int *request_info_user();
+int request_info_user();
+int revoked_certificate();
+int signing_document();
+int validate_document();
 
 #endif
